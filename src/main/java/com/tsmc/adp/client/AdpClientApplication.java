d35d6a2617f57c6609d205f637c1453c88cdebc0
JerryTang611 <jytang@gmail.com>
2022-02-17 09:33:47 +0800

    init

---
+++ src/main/java/com/tsmc/adp/client/AdpClientApplication.java
@@ -0,0 +1,13 @@
+package com.tsmc.adp.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdpClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tsmc/adp/client/AdpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsmc.adp.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
